a894f7557a837071f2cdbec89875131b58c3aeb8
Di <d.akhmetzyanov.stc@innopolis.ru>
2017-08-02 21:12:25 +0300

    Сделано hello world web приложение. Добавлена БД.

---
+++ src/main/java/ru/service/taxi/controller/MainController.java
@@ -0,0 +1,24 @@
+package ru.service.taxi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by Di on 27.07.2017.
+ */
+@Controller
+public class MainController
+{
+
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showDrivers()
+    {
+        return "main";
+    }
+
+
+
+
+}
